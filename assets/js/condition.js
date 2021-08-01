62640352156d0d45171c9ba98e8b93c4267e8519
saifur550 <isaifur550@icloud.com>
2021-08-01 19:35:05 +0800

    js class

--- assets/js/condition.js
+++ assets/js/condition.js
@@ -1,39 +1,59 @@
-var isFoodReay1 = true;
-var isFoodReay2 = false;
+// var isFoodReay1 = true;
+// var isFoodReay2 = false;
 
 
-if(isFoodReay1== true){
-    console.log("condition is working");
-}
+// // if(isFoodReay1== true){
+// //     console.log("condition is working");
+// // }
 
 
-if(isFoodReay2 == true){
-    console.log("condition is working");
-}
+// // if(isFoodReay2 == true){
+// //     console.log("condition is working");
+// // }
 
 
-var iphonePrice1 = 70000;
-var myBudget1 = 50000;
-if (iphonePrice1 < myBudget1){
-    console.log("out of budget");
-}
+// // var iphonePrice1 = 70000;
+// // var myBudget1 = 50000;
+// // if (iphonePrice1 < myBudget1){
+// //     console.log("out of budget");
+// // }
 
 
-var iphonePrice2 = 70000;
-var myBudget2 = 150000;
-if (iphonePrice2 < myBudget2 ){
-    console.log("able to purchase new iphone");
-}
-
-
-
-var iphonePrice2 = 70000;
-var myBudget2 = 15000;
-if (iphonePrice2 < myBudget2 ){
-    console.log("able to purchase new iphone");
-}else{
-    console.log("mybudget is low , so unable to purchase the iphone");
-}
+// // var iphonePrice2 = 70000;
+// // var myBudget2 = 150000;
+// // if (iphonePrice2 < myBudget2 ){
+// //     console.log("able to purchase new iphone");
+// // }
 
 
 
+// // var iphonePrice2 = 70000;
+// // var myBudget2 = 15000;
+// // if (iphonePrice2 < myBudget2 ){
+// //     console.log("able to purchase new iphone");
+// // }else{
+// //     console.log("mybudget is low , so unable to purchase the iphone");
+// // }
+
+
+
+// // var resultOut =prompt("Enter Your Number");
+
+// var  fruits = ["I", "will", "become", "a",  "developer", "inshallah"];
+// var newItem= fruits.indexOf("developer")
+// console.log(newItem);
+
+
+
+// var myFish = ['angel', 'clown', 'mandarin', 'surgeon'];
+// myFish.shift(1);
+// console.log(myFish);
+
+
+// var nums1 = [1, 5, 8];
+// nums.indexOf(0);
+// nums1.indexOf(9);
+// console.log(nums1);
+var a= 6;
+var b = 6
+console.log(a!=b);
