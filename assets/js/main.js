c32cea66c36e5ca761d6ca7379252e787ec01089
saifur550 <srstap550@gmail.com>
2021-08-01 08:17:14 +0800

    git init

---
+++ assets/js/main.js
@@ -0,0 +1,59 @@
+
+
+//============ Arry=================
+
+var userName1 = [ 'james', 'Doni', 'david', ' ben stock'];
+console.log(userName1);
+console.log(userName1.length);
+console.log(userName1[2]);
+
+
+
+//============ Arry change =================
+
+var userName2 = [ 'james', 'Doni', 'david', ' ben stock'];
+userName2[0] = "rocky";
+console.log(userName2);
+console.log(userName2.length);
+
+
+
+//============ Arry push =================
+var userName3 = [ 'james', 'Doni', 'david', ' ben stock'];
+userName3[0] = "rocky";
+userName3.push("rahul");
+userName3.push("kader");
+console.log(userName3);
+console.log(userName3.length);
+
+
+
+//============ Arry pop =================
+var userName3 = [ 'james', 'Doni', 'david', ' ben stock'];
+userName3[0] = "rocky";
+userName3.push("rahul");
+userName3.pop();
+userName3.pop();
+userName3.pop();
+userName3.pop();
+userName3.pop();
+console.log(userName3);
+console.log(userName3.length);
+
+
+
+//============ Arry shift =================
+var userName3 = [ 'james', 'Doni', 'david', ' ben stock'];
+userName3[0] = "rocky";
+userName3.push("rahul");
+userName3.pop();
+userName3.pop();
+userName3.pop();
+userName3.pop();
+userName3.pop();
+console.log(userName3);
+console.log(userName3.length);
+
+
+
+
