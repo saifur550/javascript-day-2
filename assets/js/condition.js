6bf583f117e6228a05245d8db983b5956adff14c
saifur550 <srstap550@gmail.com>
2021-08-01 08:59:36 +0800

    condition js

---
+++ assets/js/condition.js
@@ -0,0 +1,26 @@
+var isFoodReay1 = true;
+var isFoodReay2 = false;
+
+
+if(isFoodReay1== true){
+    console.log("condition is working");
+}
+
+
+if(isFoodReay2 == true){
+    console.log("condition is working");
+}
+
+
+var iphonePrice1 = 70000;
+var myBudget1 = 50000;
+if (iphonePrice1 < myBudget1){
+    console.log("out of budget");
+}
+
+
+var iphonePrice2 = 70000;
+var myBudget2 = 150000;
+if (iphonePrice2 < myBudget2 ){
+    console.log("able to purchase new iphone");
+}
---
+++ assets/js/coparison.js
@@ -0,0 +1,12 @@
+var num1 = 5;
+var num2 = 6;
+
+console.log(num1>num2);
+console.log(num1<num2);
+console.log(num1==num2);
+console.log(num1===num2);
+console.log(num1!==num2);
+
+// var enterMyLover = prompt("Enter My Number");
+// var enterYourLover = prompt("Enter Your Number");
+// console.log(enterMyLover>enterYourLover);
